4119ae063385edd340f3cfa4ae519237a74435ed
Andreas Muehlmann <andi.muehlmann@arcor.de>
2024-10-19 19:05:00 +0200

    distributiv law

--- app/Simplify.hs
+++ app/Simplify.hs
@@ -9,6 +9,7 @@ import Data.Foldable (maximumBy)
 import Data.Maybe (maybeToList)
 import Data.Ord (comparing)
 import qualified Parse as P
+import qualified Type.Reflection as P
 
 type RuleApplication = (P.BoolExpr, String)
 
@@ -45,20 +46,20 @@ doubleNegation :: P.BoolExpr -> Maybe P.BoolExpr
 doubleNegation (P.Negation (P.Negation boolExpr)) = Just boolExpr
 doubleNegation boolExpr = Nothing
 
-komplement :: P.BoolExpr -> Maybe P.BoolExpr
-komplement (P.Conjunction (P.Negation leftBoolExpr) rightBoolExpr)
+complement :: P.BoolExpr -> Maybe P.BoolExpr
+complement (P.Conjunction (P.Negation leftBoolExpr) rightBoolExpr)
   | leftBoolExpr == rightBoolExpr = Just $ P.Constant False
   | otherwise = Nothing
-komplement (P.Conjunction leftBoolExpr (P.Negation rightBoolExpr))
+complement (P.Conjunction leftBoolExpr (P.Negation rightBoolExpr))
   | leftBoolExpr == rightBoolExpr = Just $ P.Constant False
   | otherwise = Nothing
-komplement (P.Disjunction (P.Negation leftBoolExpr) rightBoolExpr)
+complement (P.Disjunction (P.Negation leftBoolExpr) rightBoolExpr)
   | leftBoolExpr == rightBoolExpr = Just $ P.Constant True
   | otherwise = Nothing
-komplement (P.Disjunction leftBoolExpr (P.Negation rightBoolExpr))
+complement (P.Disjunction leftBoolExpr (P.Negation rightBoolExpr))
   | leftBoolExpr == rightBoolExpr = Just $ P.Constant True
   | otherwise = Nothing
-komplement boolExpr = Nothing
+complement boolExpr = Nothing
 
 absorption :: P.BoolExpr -> Maybe P.BoolExpr
 absorption (P.Disjunction boolExprA (P.Conjunction boolExprB boolExprC))
@@ -75,17 +76,37 @@ absorption (P.Conjunction (P.Disjunction boolExprB boolExprC) boolExprA)
   | otherwise = Nothing
 absorption boolExpr = Nothing
 
-kommutativ :: P.BoolExpr -> Maybe P.BoolExpr
-kommutativ (P.Conjunction leftBoolExpr rightBoolExpr) = Just $ P.Conjunction rightBoolExpr leftBoolExpr
-kommutativ (P.Disjunction leftBoolExpr rightBoolExpr) = Just $ P.Disjunction rightBoolExpr leftBoolExpr
-kommutativ boolExpr = Nothing
+kommutativity :: P.BoolExpr -> Maybe P.BoolExpr
+kommutativity (P.Conjunction leftBoolExpr rightBoolExpr) = Just $ P.Conjunction rightBoolExpr leftBoolExpr
+kommutativity (P.Disjunction leftBoolExpr rightBoolExpr) = Just $ P.Disjunction rightBoolExpr leftBoolExpr
+kommutativity boolExpr = Nothing
 
-assoziativ :: P.BoolExpr -> Maybe P.BoolExpr
-assoziativ (P.Conjunction leftBoolExpr (P.Conjunction rightLeftBoolExpr rightRightBoolExpr)) = Just $ P.Conjunction (P.Conjunction leftBoolExpr rightLeftBoolExpr) rightRightBoolExpr
-assoziativ (P.Conjunction (P.Conjunction leftLeftBoolExpr leftRightBoolExpr) rightBoolExpr) = Just $ P.Conjunction leftLeftBoolExpr (P.Conjunction leftRightBoolExpr rightBoolExpr)
-assoziativ (P.Disjunction leftBoolExpr (P.Disjunction rightLeftBoolExpr rightRightBoolExpr)) = Just $ P.Disjunction (P.Disjunction leftBoolExpr rightLeftBoolExpr) rightRightBoolExpr
-assoziativ (P.Disjunction (P.Disjunction leftLeftBoolExpr leftRightBoolExpr) rightBoolExpr) = Just $ P.Disjunction leftLeftBoolExpr (P.Disjunction leftRightBoolExpr rightBoolExpr)
-assoziativ boolExpr = Nothing
+assoziativity :: P.BoolExpr -> Maybe P.BoolExpr
+assoziativity (P.Conjunction leftBoolExpr (P.Conjunction rightLeftBoolExpr rightRightBoolExpr)) = Just $ P.Conjunction (P.Conjunction leftBoolExpr rightLeftBoolExpr) rightRightBoolExpr
+assoziativity (P.Conjunction (P.Conjunction leftLeftBoolExpr leftRightBoolExpr) rightBoolExpr) = Just $ P.Conjunction leftLeftBoolExpr (P.Conjunction leftRightBoolExpr rightBoolExpr)
+assoziativity (P.Disjunction leftBoolExpr (P.Disjunction rightLeftBoolExpr rightRightBoolExpr)) = Just $ P.Disjunction (P.Disjunction leftBoolExpr rightLeftBoolExpr) rightRightBoolExpr
+assoziativity (P.Disjunction (P.Disjunction leftLeftBoolExpr leftRightBoolExpr) rightBoolExpr) = Just $ P.Disjunction leftLeftBoolExpr (P.Disjunction leftRightBoolExpr rightBoolExpr)
+assoziativity boolExpr = Nothing
+
+distributivityExpanding :: P.BoolExpr -> Maybe P.BoolExpr
+distributivityExpanding (P.Conjunction leftBoolExpr (P.Disjunction rightLeftBoolExpr rightRightBoolExpr)) = Just $ P.Disjunction (P.Conjunction leftBoolExpr rightLeftBoolExpr) (P.Conjunction leftBoolExpr rightRightBoolExpr)
+distributivityExpanding (P.Disjunction leftBoolExpr (P.Conjunction rightLeftBoolExpr rightRightBoolExpr)) = Just $ P.Conjunction (P.Disjunction leftBoolExpr rightLeftBoolExpr) (P.Disjunction leftBoolExpr rightRightBoolExpr)
+distributivityExpanding boolExpr = Nothing
+
+distributivityFactoring :: P.BoolExpr -> Maybe P.BoolExpr
+distributivityFactoring (P.Conjunction (P.Disjunction leftLeftBoolExpr leftRightBoolExpr) (P.Disjunction rightLeftBoolExpr rightRightBoolExpr))
+  | leftLeftBoolExpr == rightLeftBoolExpr = Just $ P.Disjunction leftLeftBoolExpr (P.Conjunction leftRightBoolExpr rightRightBoolExpr)
+  | leftLeftBoolExpr == rightRightBoolExpr = Just $ P.Disjunction leftLeftBoolExpr (P.Conjunction leftRightBoolExpr rightLeftBoolExpr)
+  | leftRightBoolExpr == rightLeftBoolExpr = Just $ P.Disjunction leftRightBoolExpr (P.Conjunction leftLeftBoolExpr rightRightBoolExpr)
+  | leftRightBoolExpr == rightRightBoolExpr = Just $ P.Disjunction leftRightBoolExpr (P.Conjunction leftLeftBoolExpr rightLeftBoolExpr)
+  | otherwise = Nothing
+distributivityFactoring (P.Disjunction (P.Conjunction leftLeftBoolExpr leftRightBoolExpr) (P.Conjunction rightLeftBoolExpr rightRightBoolExpr))
+  | leftLeftBoolExpr == rightLeftBoolExpr = Just $ P.Conjunction leftLeftBoolExpr (P.Disjunction leftRightBoolExpr rightRightBoolExpr)
+  | leftLeftBoolExpr == rightRightBoolExpr = Just $ P.Conjunction leftLeftBoolExpr (P.Disjunction leftRightBoolExpr rightLeftBoolExpr)
+  | leftRightBoolExpr == rightLeftBoolExpr = Just $ P.Conjunction leftRightBoolExpr (P.Disjunction leftLeftBoolExpr rightRightBoolExpr)
+  | leftRightBoolExpr == rightRightBoolExpr = Just $ P.Conjunction leftRightBoolExpr (P.Disjunction leftLeftBoolExpr rightLeftBoolExpr)
+  | otherwise = Nothing
+distributivityFactoring boolExpr = Nothing
 
 applyRule :: (P.BoolExpr -> Maybe P.BoolExpr) -> P.BoolExpr -> Maybe P.BoolExpr
 applyRule rule boolExpr =
@@ -121,8 +142,10 @@ allRuleApplicationsChangingRules :: P.BoolExpr -> [RuleApplication]
 allRuleApplicationsChangingRules = allRuleApplicationsForRules changingRules
   where
     changingRules =
-      [ (kommutativ, "kommutativ"),
-        (assoziativ, "assoziativ")
+      [ (kommutativity, "kommutativity"),
+        (assoziativity, "assoziativity"),
+        (distributivityFactoring, "distributivity-factoring"),
+        (distributivityExpanding, "distributivity-expanding")
       ]
 
 complexity :: P.BoolExpr -> Int
@@ -143,7 +166,7 @@ applySimplifyingRules boolExpr = case applyRules simplifyingRules boolExpr of
         (duality, "duality"),
         (doubleNegation, "double negation"),
         (idempotence, "idempotence"),
-        (komplement, "komplement"),
+        (complement, "complement"),
         (absorption, "absorption")
       ]
 
@@ -180,9 +203,9 @@ simplify depth boolExpr
       _ -> fst $ last simplifyingRulesApplied
     simplifications = map (\(boolExpr, ruleName) -> (boolExpr, ruleName) : simplify (depth + 1) boolExpr) (allRuleApplicationsChangingRules boolExprToSimplify)
 
-printSimplificationWithInitialExpr :: P.BoolExpr -> [(P.BoolExpr, String)] -> IO ()
+printSimplificationWithInitialExpr :: P.BoolExpr -> Simplification -> IO ()
 printSimplificationWithInitialExpr initialBoolExpr simplification = print initialBoolExpr >> printSimplification simplification
 
-printSimplification :: [(P.BoolExpr, String)] -> IO ()
+printSimplification :: Simplification -> IO ()
 printSimplification [] = return ()
 printSimplification ((boolExpr, ruleName) : xs) = putStr (ruleName ++ " -> ") >> print boolExpr >> printSimplification xs
